d33d077e63f7035ec20a06bc5e41e2e5b245cc7a
Matias Manavella <67109879+manavellamnimble@users.noreply.github.com>
2025-09-22 18:10:58 -0300

    Update user_data.sh

--- terragrunt/modules/ec2-wrapper/user_data.sh
+++ terragrunt/modules/ec2-wrapper/user_data.sh
@@ -2,6 +2,8 @@
 yum update -y
 yum install -y httpd
 
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
 # Configure httpd to run on custom port
 sed -i "s/Listen 80/Listen 8080/" /etc/httpd/conf/httpd.conf
 
